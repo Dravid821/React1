b85c040933fda41abf562040bb7527a5279ebdeb
Dravid821 <124051008+Dravid821@users.noreply.github.com>
2023-04-17 18:27:12 +0530

    new

--- src/Components/Datafetch.js
+++ src/Components/Datafetch.js
@@ -1,87 +1,79 @@
-import { useEffect, useState } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Components/Datafetch.scss";
-import { Button } from "bootstrap";
-// UseState
-export default function Datafetch() {
-  const [data, setdata] = useState([]);
-  const [filterdata, setfilterdata] = useState([]);
-  const [selectdata, setSelectdata] = useState("");
-  const [searchdata, setsearchdata] = useState("");
-  const [clear, setCleardata] = useState(false);
-  // const [matchdata, setmatchdata] = useState([])
 
-  // Data Fetching
+export default function Datafetch() {
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [selectData, setSelectData] = useState("");
+  const [searchData, setSearchData] = useState("");
+  const [clear, setClearData] = useState(false);
 
   const fetchData = async () => {
-    let result = await axios.get(
-      "https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json"
-    );
-    let filterdata = result.data.slice(1);
-    if (selectdata !== "") {
-      filterdata = filterdata.filter((data) => data.platform === selectdata);
-    }
-    if (searchdata !== "") {
-      filterdata = filterdata.filter((data) =>
-        data.title.toLowerCase().includes(searchdata.toLowerCase())
+    try {
+      const response = await axios.get(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json"
       );
+      setData(response.data.slice(1));
+    } catch (error) {
+      console.log(error);
     }
-    setfilterdata(filterdata);
-    setdata(filterdata);
   };
-  // UseEffect Calling Function
+
   useEffect(() => {
     fetchData();
-  });
-  //
+  }, []);
+
+  useEffect(() => {
+    let filteredData = [...data];
+    if (selectData !== "") {
+      filteredData = filteredData.filter((d) => d.platform === selectData);
+    }
+    if (searchData !== "") {
+      filteredData = filteredData.filter((d) =>
+        d.title.toLowerCase().includes(searchData.toLowerCase())
+      );
+    }
+    setFilterData(filteredData);
+  }, [selectData, searchData, data]);
+
   const handlePlatformChange = (event) => {
-    setSelectdata(event.target.value);
+    setSelectData(event.target.value);
   };
-  //Platform Serach Funcnality
-  const handlePlatformSearh = (event) => {
-    event.preventDefault();
-    setsearchdata(event.target.value);
-    setCleardata(true);
+
+  const handlePlatformSearch = (event) => {
+    setSearchData(event.target.value);
+    setClearData(true);
   };
-  // Input Click Funcnality
+
   const handleClick = (title) => {
-    setsearchdata(title);
-    setCleardata(false);
+    setSearchData(title);
+    setClearData(false);
   };
-  const searchItems = () => {
-    const newdata = newdata.filter((data) =>
-      data.title.toLowerCase().includes(searchdata.toLowerCase())
-    );
-    setdata(newdata);
-  };
+
   return (
-    //render part
     <div className="container main-list">
       <div className="row ">
-        {/* Input Button */}
         <div className="input-group mt-3 col-12 col-md-12 col-sm-12">
           <input
             type="search"
-            value={searchdata}
-            onChange={handlePlatformSearh}
+            value={searchData}
+            onChange={handlePlatformSearch}
             className="form-control rounded"
             placeholder="Search"
           />
-          {/* <button value={searchdata} type="submit" onClick={() => searchItems} className="btn btn-outline-primary">search</button> */}
         </div>
-        {/* Input Button Click to show list Funcnality */}
         <div className="mt-3 col-12 col-md-12 col-sm-12 list">
           {clear &&
-            searchdata &&
-            data.map((data) => (
-              <div className="col-12">
+            searchData &&
+            data.map((d) => (
+              <div className="col-12" key={d.id}>
                 <li
                   className="a"
-                  onClick={() => handleClick(data.title)}
-                  value={data.title}
+                  onClick={() => handleClick(d.title)}
+                  value={d.title}
                 >
-                  {data.title}
+                  {d.title}
                 </li>
               </div>
             ))}
@@ -95,22 +87,20 @@ export default function Datafetch() {
             className="form-select mt-4"
             aria-label="Default select example"
           >
-            <>
-              <option value="">All</option>
-              <option value="PlayStation Vita">PlayStation Vita</option>
-              <option value="PlayStation 3">PlayStation 3</option>
-              <option value="iPad">iPad</option>
-              <option value="Xbox 360">Xbox 360</option>
-              <option value="Macintosh">Macintosh</option>
-              <option value="PC">PC</option>
-              <option value="iPhone">iPhone</option>
-              <option value="Nintendo DS">Nintendo DS</option>
-            </>
+            <option value="">All</option>
+            <option value="PlayStation Vita">PlayStation Vita</option>
+            <option value="PlayStation 3">PlayStation 3</option>
+            <option value="iPad">iPad</option>
+            <option value="Xbox 360">Xbox 360</option>
+            <option value="Macintosh">Macintosh</option>
+            <option value="PC">PC</option>
+            <option value="iPhone">iPhone</option>
+            <option value="Nintendo DS">Nintendo DS</option>
           </select>
         </div>
         <div className="row">
-          {filterdata &&
-            data.map((item) => (
+          {filterData  &&
+            filterData.map((item) => (
               <div className="col-12 col-md-6 col-lg-4 cl mt-4" key={item.id}>
                 <div className="card">
                   <div className="card-body">
@@ -131,18 +121,17 @@ export default function Datafetch() {
                         <span>score :</span>
                         {item.score}
                       </button>
-                      {
-                        item.editors_choice == "N" ?
+                      {item.editors_choice == "N" ? (
                         <button className="card-text btn btn1 btn-danger">
                           <span>editors_choice :</span>
                           {item.editors_choice}
                         </button>
-                      :         
-                      <button className="card-text btn btn1 btn-success">
-                      <span>editors_choice :</span>
-                      {item.editors_choice}
-                    </button>
-                    }
+                      ) : (
+                        <button className="card-text btn btn1 btn-success">
+                          <span>editors_choice :</span>
+                          {item.editors_choice}
+                        </button>
+                      )}
                     </div>
                   </div>
                 </div>
